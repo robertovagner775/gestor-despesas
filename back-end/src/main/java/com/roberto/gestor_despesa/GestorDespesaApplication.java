49644b4913adaeb5df63e2206f9e575a4ba72b59
Roberto Vagner <53801038+robertovagner775@users.noreply.github.com>
2025-10-19 23:02:03 -0300

    Criando estrutura básica do projeto

---
+++ back-end/src/main/java/com/roberto/gestor_despesa/GestorDespesaApplication.java
@@ -0,0 +1,13 @@
+package com.roberto.gestor_despesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDespesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDespesaApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/roberto/gestor_despesa/GestorDespesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roberto.gestor_despesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDespesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
